950ca48957813d8811d1b8e8d4762f71e5424db7
Ybrayym <ybrayymjan90@gmail.com>
2020-06-09 01:00:21 -0400

    added the views for tweet

--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from tweet.models import Tweet
+from tweet.forms import TweetForm
+from twitteruser.models import TwitterUser
+
 
 # Create your views here.
+
+
+def post_tweet(request):
+    if request.method == "POST":
+        form = TweetForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            twitteruser = TwitterUser.objects.get(id=request.user.id)
+            message_box = Tweet.objects.create(
+                message_box=data['message_box'],
+                twitteruser=twitteruser
+            )
+        return HttpResponseRedirect(reverse('homepage'))
+    form = TweetForm()
+    return render(request, 'tweet.html', {'form': form})
+
+
+def main(request):
+    data = Tweet.objects.all()
+    return render(request, 'main.html', {'data': data})
